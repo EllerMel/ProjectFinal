9e580c18d620bd156d3503b25931c9a50d5bd95d
M Eller <msmaedev@gmail.com>
2019-04-24 13:31:54 -0400

    Horse Edit Cancel Button

--- resources/views/horses/edit.blade.php
+++ resources/views/horses/edit.blade.php
@@ -91,25 +91,41 @@
             <br>
 
         <br>
-        <div class="field">
-            <div class="control">
-                <button type="submit" class="btn btn-purple">Update Horse</button>
+        <div class="row">
+            <div class="col">
+                <div class="field">
+                    <div class="control">
+                        <button type="submit" class="btn btn-purple btn-main">Update Horse</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="field">
+                    <div class="control">
+                        <a href="/horses" class="btn btn-secondary btn-main">Cancel</a>
+                    </div>
+                </div>
+            </div>
+    </form>            
+            <div class="col">
+                <form method="POST" action="/horses/{{ $horse->id }}">
+                @method('DELETE')
+                @csrf       
+
+                    <div class="field">
+                        <div class="control">
+                            <button type="submit" class="btn btn-danger btn-main">Delete Horse</button>
+                        </div>
+                    </div>
+                 </form>
             </div>
         </div>
+
     
-    </form>
+
 
     <br>
 
-    <form method="POST" action="/horses/{{ $horse->id }}">
-        @method('DELETE')
-        @csrf       
 
-        <div class="field">
-            <div class="control">
-                <button type="submit" class="btn btn-secondary">Delete horse</button>
-            </div>
-        </div>
-    </form>
 </div>
 @endsection

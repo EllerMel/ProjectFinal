ecdfd92980c5d99cd0e2ed450463a6cf315712af
M Eller <msmaedev@gmail.com>
2019-05-06 15:22:16 -0400

    Additional Users Indiv Seeded

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -100,5 +100,50 @@ class AdminSeeder extends Seeder
             'admin' => 0,
             'approved_at' => now(),
         ]);
+
+        \App\User::create([
+            'name' => 'Chelsea',
+            'email' => 'chelsea@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('chelseachelsea'),
+            'admin' => 0,
+            'approved_at' => now(),
+        ]);
+
+        \App\User::create([
+            'name' => 'Jocelyn',
+            'email' => 'jocelyn@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('jocelynjocelyn'),
+            'admin' => 0,
+            'approved_at' => now(),
+        ]);
+
+        \App\User::create([
+            'name' => 'Olivia',
+            'email' => 'olivia@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('oliviaolivia'),
+            'admin' => 0,
+            'approved_at' => now(),
+        ]);
+
+        \App\User::create([
+            'name' => 'Jen',
+            'email' => 'jen@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('jenjenjen'),
+            'admin' => 0,
+            'approved_at' => now(),
+        ]);
+
+        \App\User::create([
+            'name' => 'Kathleen',
+            'email' => 'kathleen@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('kathleenkathleen'),
+            'admin' => 0,
+            'approved_at' => now(),
+        ]);
     }
 }
--- database/seeds/IndivSeeder.php
+++ database/seeds/IndivSeeder.php
@@ -14,45 +14,45 @@ class IndivSeeder extends Seeder
         \App\Individual::create([
             'displayName' => 'Mo',
             'user_id' => "1",
-            'phoneNumber' => '555-555-5555',
+            'phoneNumber' => '248-555-4444',
             'isInstructor' => "1"
         ]);
 
         \App\Individual::create([
             'displayName' => 'Mel',
             'user_id' => "2",
-            'phoneNumber' => '333-333-3333',
+            'phoneNumber' => '248-444-5555',
             'isInstructor' => "1"
         ]);
 
         \App\Individual::create([
             'displayName' => 'Lyss',
             'user_id' => "3",
-            'phoneNumber' => '555-555-5555',
+            'phoneNumber' => '248-777-9999',
             'emergencyContact' => 'Father',
-            'emergencyPhone' => '444-444-4444'
+            'emergencyPhone' => '313-444-4444'
         ]);
 
         \App\Individual::create([
             'displayName' => 'Steph',
             'user_id' => "4",
-            'phoneNumber' => '333-333-3333',
+            'phoneNumber' => '248-333-4444',
             'emergencyContact' => 'Mother',
-            'emergencyPhone' => '222-222-2222'
+            'emergencyPhone' => '248-222-2222'
         ]);
 
         \App\Individual::create([
             'displayName' => 'Tori',
             'user_id' => "5",
-            'phoneNumber' => '111-111-1111',
-            'emergencyContact' => 'Sister',
-            'emergencyPhone' => '222-222-2222'
+            'phoneNumber' => '574-111-1111',
+            'emergencyContact' => 'Larry',
+            'emergencyPhone' => '502-222-2222'
         ]);
 
         \App\Individual::create([
             'displayName' => 'Mal',
             'user_id' => "6",
-            'phoneNumber' => '333-333-3333',
+            'phoneNumber' => '248-444-3333',
             'emergencyContact' => 'Eddie',
             'emergencyPhone' => '444-444-4444'
         ]);
@@ -60,7 +60,7 @@ class IndivSeeder extends Seeder
         \App\Individual::create([
             'displayName' => 'Tressa',
             'user_id' => "7",
-            'phoneNumber' => '555-555-5555',
+            'phoneNumber' => '423-555-5555',
             'emergencyContact' => 'Mother',
             'emergencyPhone' => '777-777-7777'
         ]);
@@ -68,7 +68,7 @@ class IndivSeeder extends Seeder
         \App\Individual::create([
             'displayName' => 'Ash',
             'user_id' => "8",
-            'phoneNumber' => '888-888-8888',
+            'phoneNumber' => '248-888-8888',
             'emergencyContact' => 'Char',
             'emergencyPhone' => '999-999-9999'
         ]);
@@ -77,16 +77,56 @@ class IndivSeeder extends Seeder
             'displayName' => 'Char',
             'user_id' => "9",
             'phoneNumber' => '999-999-9999',
-            'emergencyContact' => 'Char',
-            'emergencyPhone' => '888-888-8888'
+            'emergencyContact' => 'Ash',
+            'emergencyPhone' => '248-888-8888'
         ]);
 
         \App\Individual::create([
             'displayName' => 'Jessica',
             'user_id' => "10",
-            'phoneNumber' => '000-000-0000',
+            'phoneNumber' => '859-000-0000',
             'emergencyContact' => 'Mother',
             'emergencyPhone' => '110-110-1100'
+        ]);
+
+        \App\Individual::create([
+            'displayName' => 'Chelsea',
+            'user_id' => "11",
+            'phoneNumber' => '859-222-333',
+            'emergencyContact' => 'Mark',
+            'emergencyPhone' => '333-222-1111'
+        ]);
+
+        \App\Individual::create([
+            'displayName' => 'Jocelyn',
+            'user_id' => "12",
+            'phoneNumber' => '607-555-7777',
+            'emergencyContact' => 'Brother',
+            'emergencyPhone' => '777-555-4444'
+        ]);
+
+        \App\Individual::create([
+            'displayName' => 'Olivia',
+            'user_id' => "13",
+            'phoneNumber' => '859-999-0000',
+            'emergencyContact' => 'Scotty',
+            'emergencyPhone' => '000-999-8888'
+        ]);
+
+        \App\Individual::create([
+            'displayName' => 'Jen',
+            'user_id' => "14",
+            'phoneNumber' => '517-000-999',
+            'emergencyContact' => 'Pam',
+            'emergencyPhone' => '888-777-5555'
+        ]);
+
+        \App\Individual::create([
+            'displayName' => 'Kathleen',
+            'user_id' => "15",
+            'phoneNumber' => '859-111-0000',
+            'emergencyContact' => 'Mother',
+            'emergencyPhone' => '111-000-1111'
         ]);
     }
 }
